eadf0626c44a193743fc2c22b447e81dc64ee1f5
coleheth <cjhether3@gmail.com>
2026-05-01 08:50:39 -0400

    state machine based status led update to reduce hardware calls

--- src/subsystems/statusLed/statusLed.cpp
+++ src/subsystems/statusLed/statusLed.cpp
@@ -1,17 +1,12 @@
 #include "statusLed.hpp"
 #include <Arduino.h>
 
-#define RED_PIN 10
-#define BLUE_PIN 9
-#define GREEN_PIN 11
-
-#define LED_BRIGHTNESS 20 // Out of 255
-#define LED_LIGHT_MIN_TIMING 500
-
-unsigned long last_update;
-unsigned long start_of_current_pattern;
-bool has_run_pattern = false;
-StatusLEDMode current_mode;
+void StatusLEDSubsystem::setColor(float r, float g, float b)
+{
+    analogWrite(RED_PIN, r * LED_BRIGHTNESS);
+    analogWrite(BLUE_PIN, b * LED_BRIGHTNESS);
+    analogWrite(GREEN_PIN, g * LED_BRIGHTNESS);
+}
 
 void StatusLEDSubsystem::setup()
 {
@@ -19,60 +14,68 @@ void StatusLEDSubsystem::setup()
     pinMode(BLUE_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
 
+    setColor(0, 0, 1);
+
     last_update = millis();
 }
 
-void updateMode(int i)
+void StatusLEDSubsystem::updateFromQueue()
 {
-    if (i == 0)
-        current_mode = OK;
-    else if (i == 1)
-        current_mode = LOOP_OVERRUN;
-    else
-        current_mode = UNKNOWN_ERROR;
-}
+    Event event;
 
-void setColor(float r, float g, float b)
-{
-    analogWrite(RED_PIN, r * LED_BRIGHTNESS);
-    analogWrite(BLUE_PIN, b * LED_BRIGHTNESS);
-    analogWrite(GREEN_PIN, g * LED_BRIGHTNESS);
+    if (popEvent(event, EventType::EVENT_STATUS_UPDATE))
+    {
+        state = event.data;
+        start_of_current_pattern = millis();
+        pattern_ran = false;
+        pattern_ran_b = false;
+    }
+    last_update = millis();
 }
 
 void StatusLEDSubsystem::loop()
 {
-    Event event;
+    if (millis() - last_update > UPDATE_INTERVAL)
+        updateFromQueue();
 
-    // deal with new patterns
-    if (millis() - last_update > LED_LIGHT_MIN_TIMING)
+    switch (state)
     {
-        if (popEvent(event, EventType::EVENT_STATUS_UPDATE))
+    case SLED_SHOWING_OK:
+        if (!pattern_ran)
         {
-            updateMode(event.data);
-            start_of_current_pattern = millis();
-            has_run_pattern = true;
+            setColor(0, 1, 0);
+            pattern_ran = true;
         }
-        last_update = millis();
-    }
+        break;
 
-    // deal with pattern
-    if (current_mode == OK)
-    {
-        setColor(0, 1, 0);
-    }
-    else if (current_mode == LOOP_OVERRUN)
-    {
+    case SLED_SHOWING_LOOP_OVERRUN:
         if ((millis() - start_of_current_pattern) % 500 < 250)
         {
-            setColor(1, 0, 1);
+            if (!pattern_ran)
+            {
+                setColor(1, 0, 1);
+                pattern_ran = true;
+                pattern_ran_b = false;
+            }
         }
         else
         {
-            setColor(0, 1, 1);
+            if (!pattern_ran_b)
+            {
+                setColor(0, 1, 1);
+                pattern_ran_b = true;
+                pattern_ran = false;
+            }
         }
-    }
-    else if (current_mode == UNKNOWN_ERROR)
-    {
-        setColor(1, 0, 0);
-    }
+        break;
+
+    case SLED_SHOWING_GEN_ERROR:
+        if (!pattern_ran)
+        {
+            setColor(1, 0, 0);
+            pattern_ran = true;
+        }
+        break;
+
+    } // end of switch btw
 }
--- src/subsystems/statusLed/statusLed.hpp
+++ src/subsystems/statusLed/statusLed.hpp
@@ -3,18 +3,36 @@
 #include "../../core/event.hpp"
 #include <Arduino.h>
 
-enum StatusLEDMode {
-    OK,
-    UNKNOWN_ERROR,
-    LOOP_OVERRUN
+#define RED_PIN 10
+#define BLUE_PIN 9
+#define GREEN_PIN 11
+
+#define LED_BRIGHTNESS 20 // Out of 255
+
+#define UPDATE_INTERVAL 500
+
+enum StatusLEDState
+{
+    SLED_SHOWING_OK,
+    SLED_SHOWING_LOOP_OVERRUN,
+    SLED_SHOWING_GEN_ERROR
 };
 
-class StatusLEDSubsystem : public Subsystem {
-    public:
-        void setup() override;
-        void loop() override;
+class StatusLEDSubsystem : public Subsystem
+{
+public:
+    void setup() override;
+    void loop() override;
 
-    private:
-        unsigned long lastToggle = 0;
-        StatusLEDMode mode = OK;
+private:
+    void updateFromQueue();
+    void setColor(float r, float g, float b);
+
+    uint32_t state = SLED_SHOWING_OK;
+
+    unsigned long last_update = 0;
+    unsigned long start_of_current_pattern;
+
+    bool pattern_ran = false;
+    bool pattern_ran_b = false;
 };
